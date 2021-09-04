9a3cbb0b79ae322e3c546764e5e9a35663260d0e
qiyuan11111 <1746257500@qq.com>
2021-09-04 23:43:08 +0800

    修改目录结构

--- tomcat/start.sh
+++ tomcat/start.sh
@@ -2,5 +2,5 @@
 chmod 777 /etc/apache2/sites-enabled/000-default.conf
 chmod 700 /usr/lib/comparator/libcmpfile.so
 service apache2 start
-/usr/local/apache-tomcat-8.5.60/bin/catalina.sh run
+/usr/local/apache-tomcat-8.5.69/bin/catalina.sh run
 /bin/bash
